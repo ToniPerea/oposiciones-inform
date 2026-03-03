868d11f2389437f4df02ac6ef384a363c4298bbb
ToniPerea <apepa31@gmail.com>
2026-03-03 21:38:14 +0100

    fix: change enviroment prod variables

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,9 +4,9 @@
 export const environment = {
   production: true,
   emailjs: {
-    publicKey: '__EMAILJS_PUBLIC_KEY__',
-    serviceId: '__EMAILJS_SERVICE_ID__',
-    confirmationTemplateId: '__EMAILJS_CONFIRMATION_TEMPLATE_ID__',
-    notificationTemplateId: '__EMAILJS_NOTIFICATION_TEMPLATE_ID__',
+    publicKey: 'EMAILJS_PUBLIC_KEY',
+    serviceId: 'EMAILJS_SERVICE_ID',
+    confirmationTemplateId: 'EMAILJS_CONFIRMATION_TEMPLATE_ID',
+    notificationTemplateId: 'EMAILJS_NOTIFICATION_TEMPLATE_ID',
   },
 };
